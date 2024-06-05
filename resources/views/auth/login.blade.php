bc9f2a9b463870742e72745bdaceb37d1209bf06
NoSineB <132141781+NoSineB@users.noreply.github.com>
2024-06-05 21:30:38 +0530

    Login Errors Message Bug Fixed

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,10 +6,12 @@
             <div>
                 <label for="email">Email</label>
                 <input id="email" name="email" type="email" placeholder="joe@example.com">
+                @error('email')<p class="ms-4 text-sm text-red-500">{{$message}}</p>@enderror
             </div>
             <div>
                 <label for="password">Password</label>
                 <input type="password" id="password" name="password">
+                @error('password')<p class="ms-4 text-sm text-red-500">{{$message}}</p>@enderror
             </div>
 
             <a href="/register" class="me-2 hover:underline">Register Now</a>
