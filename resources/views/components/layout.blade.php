56bcde92cedfdac8a64bffa2aa52e457b2bb52f4
NoSineB <132141781+NoSineB@users.noreply.github.com>
2024-06-06 15:46:09 +0530

    Font Page Design Changed

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,9 +6,10 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @vite('resources/css/app.css')
-    <title>Note Down</title>
+    <title>2Do</title>
 </head>
-<body>
+<body class="border-gray-200 flex flex-col justify-center items-center max-h-screen">
+
     {{$slot}}
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,20 @@
 <x-layout>
-    <header class="p-4">
-        <h1 class="text-3xl font-bold">
-            Hello, {{ auth()->user()->name }}
-        </h1>
-        <button type="submit" form="logout-form">Logout</button>
+    <header class="p-4 flex justify-center items-center h-[30vh]">
+        <div class="flex flex-col justify-between items-center">
+            <div class="text-gray-800">
+                <h1 class="text-8xl">2Do</h1>
+                <h2 class="text-center">Organise Yourself</h2>
+            </div>
+        </div>
     </header>
+    <form action="/todos" class="mb-20" method="post">
+        @csrf
+        <div class="w-[650px] shadow-lg rounded-xl flex justify-between items-center">
+            <input id="todo" name="todo" type="text" placeholder="Add your todo" required autocomplete="off"
+                   class=" p-4 focus-visible:outline-0 flex-1" >
+            <button class="me-2 p-2 h-[40px] w-[40px] text-3xl bg-gray-100 flex justify-center items-center rounded-lg hover:border-2">+</button>
+        </div>
+    </form>
     <div class="p-4">
         <h2 class="text-lg font-bold">To-Do</h2>
         <ul>
@@ -12,16 +22,6 @@
                 <li class="mt-2">{{$todo->title}}</li>
             @endforeach
         </ul>
-    </div>
-    <div class="p-4">
-        <form action="/todos" method="post">
-            @csrf
-            <div>
-                <input id="todo" name="todo" type="text" placeholder="Enter your todo here ...." required class="w-[600px]">
-                @error('todo')<p class="ms-4 text-sm text-red-500">{{$message}}</p>@enderror
-            </div>
-            <button type="submit">Create</button>
-        </form>
     </div>
     <form action="/logout" method="post" id="logout-form" class="hidden">
         @csrf
