27ae14cb354e430fb365c6409a32c4f608b7956f
NoSineB <132141781+NoSineB@users.noreply.github.com>
2024-06-06 07:46:49 +0530

    Auth User can view there own todos

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -3,13 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Models\Todo;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TodoController extends Controller
 {
     public function index(){
         return view('welcome',[
-            'todos' => Todo::all()
+            'todos' => Todo::query()->get()->where('user_id', Auth::id())
         ]);
     }
 }
